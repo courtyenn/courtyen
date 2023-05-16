46e16fe007249d3fa43c77317bda630085e29c1f
Courtney Nguyen <hello@courtyen.io>
2023-05-15 23:37:08 -0600

    First commit to staging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
-import { UnderConstruction } from "../components/UnderConstruction";
-
 export const Home = () => {
   return (
     <div className="grid-area-content flex flex-col gap-y-0 sm:gap-y-10">
       <h1 className="cursive text-white-900 text-center text-7xl md:text-9xl mb-14">Courtyen</h1>
-      <UnderConstruction />
       <main className="mt-0 mb-4 sm:mt-14 sm:mb-14">
         <section className="flex flex-wrap gap-x-6 gap-y-6 sm:gap-y-0 justify-center">
           <img src="images/starwars.png" alt="Photo of Courtney" className="max-w-48 max-h-48"/>
